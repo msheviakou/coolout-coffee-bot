609b0139edd9f5376a69646fe9813a9659072b64
Maksim.Sheviakov <Maksim.Sheviakov@life.com.by>
2021-05-23 19:14:34 +0300

    ejevika added

--- src/main/java/by/coolout/bot/statics/Syrups.java
+++ src/main/java/by/coolout/bot/statics/Syrups.java
@@ -5,15 +5,16 @@ import java.util.EnumSet;
 public enum Syrups {
 
     NOT_NEEDED("Не нужен"),
-    COCONUT ("Кокос"),
+//    COCONUT ("Кокос"),
     SALTED_CARAMEL ("Солёная карамель"),
-    COOKIE_WITH_CHOCOLATE ("Печенье с шоколадной крошкой"),
+//    COOKIE_WITH_CHOCOLATE ("Печенье с шоколадной крошкой"),
     HAZELNUT ("Лесной орех"),
     VANILLA ("Ваниль"),
     GINGERBREAD ("Имбирный пряник"),
     MINT ("Мята"),
     GRENADINE ("Гренадин"),
     ALMOND ("Миндаль"),
+    EJEVIKA ("Ежевика"),
     LAVENDER ("Лаванда");
 
     private String name;
